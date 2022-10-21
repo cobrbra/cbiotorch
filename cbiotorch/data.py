708425348aa1c18730a67986e86b49f3e03b3a8d
Jacob Bradley <cobrbradley@gmail.com>
2022-10-21 16:01:32 +0100

    fix: make __getitem__ return multiple mutations
    Originally this was erroneously returning a single mutation chosen by idx. Idx should select the patient, which should then determine the samples.

--- cbiotorch/data.py
+++ cbiotorch/data.py
@@ -1,7 +1,6 @@
 """ Data loaders. """
 
-from collections.abc import Callable
-from typing import Optional
+from typing import Callable, Optional
 
 from bravado.client import SwaggerClient
 from torch.utils.data import Dataset
@@ -39,7 +38,19 @@ class MutationDataset(Dataset):
         self.transform = transform
 
     def __len__(self) -> int:
+        """Returns number of samples (in this case, patients) in the dataset."""
         return len(self.patients)
 
-    def __getitem__(self, idx):
-        return self.mutations[idx]
+    def __getitem__(self, idx: int):
+        """
+        Access an individual sample in the dataset.
+        Args:
+            idx (integer): should take a value between zero and the length of the dataset
+        """
+        patient_id = self.patients[idx].patientId
+        sample = [m for m in self.mutations if m.patiendId == patient_id]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
