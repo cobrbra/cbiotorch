7d85b99078f6372cf143ee41880123885e3fe59a
Jacob Bradley <cobrbradley@gmail.com>
2022-10-21 15:28:44 +0100

    feat: create pytorch dataset class for mutation datasets
    Initialises by loading dataset from cbioportal, with methods necessary to be a valid pytorch dataset class.

---
+++ cbiotorch/data.py
@@ -0,0 +1,45 @@
+""" Data loaders. """
+
+from collections.abc import Callable
+from typing import Optional
+
+from bravado.client import SwaggerClient
+from torch.utils.data import Dataset
+
+
+class MutationDataset(Dataset):
+    """PyTorch Dataset class for cBioPortal mutation data."""
+
+    def __init__(
+        self,
+        study_id: str,
+        from_url: str = "https://www.cbioportal.org/api/v2/api-docs",
+        transform: Optional[Callable] = None,
+    ) -> None:
+        """
+        Args:
+            dataset
+
+        """
+        cbioportal = SwaggerClient.from_url(
+            from_url,
+            config={
+                "validate_requests": False,
+                "validate_responses": False,
+                "validate_swagger_spec": False,
+            },
+        )
+        self.mutations = cbioportal.Mutations.getMutationsInMolecularProfileBySampleListIDUsingGET(
+            molecularProfileID=f"{study_id}_mutations",
+            sampleListId=f"{study_id}_all",
+            projection="DETAILED",
+        ).result()
+
+        self.patients = cbioportal.Patients.getAllPatientsInStudyUsingGET(studyID=study_id).result()
+        self.transform = transform
+
+    def __len__(self) -> int:
+        return len(self.patients)
+
+    def __getitem__(self, idx):
+        return self.mutations[idx]
